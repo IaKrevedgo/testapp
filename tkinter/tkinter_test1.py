e68d1a693cae74c922d2d40893316d8b5b0b39ca
IaKrevedgo <68410799+IaKrevedgo@users.noreply.github.com>
2022-04-06 16:12:13 +0500

    work0604

---
+++ tkinter/tkinter_test1.py
@@ -0,0 +1,6 @@
+# изучаем построение графических интерфейсов с помощью TKINTER
+
+import tkinter as tk                                                                                   # подключаем библиотеку TKINTER
+
+window = tk.Tk()
+
